f14d5b5ab5eb130a64be201ee65a4cb1d49020a8
Oyerinde Samuel <53960417+sam4rano@users.noreply.github.com>
2021-06-24 11:43:59 +0100

    adding files for github pages

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './card.style.css';
+
+export const Card = (props) => (
+  <div className="card-container">
+    <img
+      className='imga'
+      alt="monster"
+      src={`https://robohash.org/CJI.png/${props.monster.id}set=set1&size=120x120`}
+    />
+    <h3> {props.monster.name} </h3>
+    <p>{props.monster.email}</p>
+  </div>
+);
